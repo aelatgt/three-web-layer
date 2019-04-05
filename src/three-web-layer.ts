8d1fc4faefa41574fe6aed87507b11fc87cb8246
Gheric Speiginer <gheric.speiginer@gmail.com>
2019-04-05 13:11:19 -0400

    fix: update state from classlist

--- src/three-web-layer.ts
+++ src/three-web-layer.ts
@@ -322,21 +322,13 @@ export default class WebLayer3D extends THREE.Object3D {
             const removedClasses = arraySubtract(oldClasses, currentClasses)
             let needsRasterize = false
             for (const c of removedClasses) {
-              if (c === 'hover') {
-                continue
-              }
-              if (layer._states[c]) {
-                layer.state = ''
+              if (c === 'hover' || layer._states[c]) {
                 continue
               }
               needsRasterize = true
             }
             for (const c of addedClasses) {
-              if (c === 'hover') {
-                continue
-              }
-              if (layer._states[c]) {
-                layer.state = c
+              if (c === 'hover' || layer._states[c]) {
                 continue
               }
               needsRasterize = true
@@ -390,12 +382,9 @@ export default class WebLayer3D extends THREE.Object3D {
   }
 
   /**
-   * Change the texture state.
+   * Get the texture state.
    * Note: if a state is not available, the `default` state will be rendered.
    */
-  set state(state: string) {
-    this._state = state
-  }
   get state() {
     return this._state
   }
@@ -584,6 +573,17 @@ export default class WebLayer3D extends THREE.Object3D {
       .split(/\s+/)
       .filter(Boolean)
     states.push('')
+
+    // set the current state from classlist
+    const classes = element.classList
+    let state = ''
+    for (const c of classes) {
+      if (states.indexOf(c) > -1) {
+        state = c
+        break
+      }
+    }
+    this._state = state
 
     // cleanup unused textures
     for (const stateKey in this._states) {
